a57e46c54e33ab6eee4c48a6e49d397702d3a448
Rodolfo <rodormz98@gmail.com>
2021-05-27 17:45:01 -0500

    Update dropdown item elements and add new one for Nueva Alianza partido

--- candidatosnl/src/components/PartidoDropdown.js
+++ candidatosnl/src/components/PartidoDropdown.js
@@ -13,12 +13,13 @@ const PartidoDropdown = (props) => {
                 <a className="dropdown-item" onClick={props.partido}>PRI</a>
                 <a className="dropdown-item" onClick={props.partido}>PRD</a>
                 <a className="dropdown-item" onClick={props.partido}>PT</a>
-                <a className="dropdown-item" onClick={props.partido}>Partido Verde</a>
-                <a className="dropdown-item" onClick={props.partido}>Movimiento Ciudadano</a>
+                <a className="dropdown-item" onClick={props.partido}>PVEM</a>
+                <a className="dropdown-item" onClick={props.partido}>MC</a>
                 <a className="dropdown-item" onClick={props.partido}>Morena</a>
                 <a className="dropdown-item" onClick={props.partido}>PES</a>
-                <a className="dropdown-item" onClick={props.partido}>Fuerza por México</a>
                 <a className="dropdown-item" onClick={props.partido}>RSP</a>
+                <a className="dropdown-item" onClick={props.partido}>Fuerza por México</a>
+                <a className="dropdown-item" onClick={props.partido}>Nueva Alianza</a>
             </div>
         </div>
         </div>
