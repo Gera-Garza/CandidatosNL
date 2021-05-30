703c6b6d073a96e71b3f32d1ff5e77395d356d6f
alfredofloresnt <alfredofloresn.t@gmail.com>
2021-05-30 16:00:19 -0500

    Added search bar

--- candidatosnl/src/components/SearchBar.js
+++ candidatosnl/src/components/SearchBar.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import './SearchBar.css'
+import { Typeahead } from 'react-bootstrap-typeahead'
 
 function SearchBar(props) {
+    let data = props.allCandidates.map(item => {
+        return item.nombre
+    })
     return (
         <div class="searchBar">
         <form class="form-inline my-2 my-lg-2">
-            <input class="form-control mr-sm-2" 
-            type="search" 
-            placeholder="Buscar candidatos..." 
-            aria-label="Search"
-            value = {props.searchTerm} 
+                    <Typeahead 
+            id="basic-typeahead-single"
+            labelKey="name"
+            options={data}
+            placeholder="Buscar candidatos..."
             onChange = {props.handleChange}
             />
+            <button className="btn btn-primary" >Buscar</button>
         </form>
         </div>
     )
