330c34208ae8f82cbb767736c81c63fb5ce070cd
Rodolfo <rodormz98@gmail.com>
2021-06-01 16:47:56 -0500

    Quick fix to typo in Acerca section

--- candidatosnl/src/components/Acerca.js
+++ candidatosnl/src/components/Acerca.js
@@ -3,7 +3,7 @@ import './Acerca.css';
 const Acerca = () => {
     return (
         <div className="container acercaContainer">
-            <h2>Acera de nosotros</h2>
+            <h2>Acerca de nosotros</h2>
             <p>Nuestro objetivo es que cualquier persona que visite nuestro portal pueda indagar más sobre los candidatos en Nuevo León, desde gobernadores, diputados y presidentes municipales, también podrán encontrar información como su trayectoria profesional, propuestas diferentes cargos que ha tenido y sus redes sociales.
             <br />
 Todo esto con el propósito para que puedan llevar un seguimiento de todo lo que han hecho los candidatos, así como todo lo que planean hacer para mejoramiento de la comunidad y en base a esto puedan tomar mejores decisiones al momento de decantarse por algún candidato</p>
